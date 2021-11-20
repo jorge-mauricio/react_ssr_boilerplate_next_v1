5bb0e6ea90b5396337da0bd6f5da84bcac7219ea
jorge-mauricio <web@jorgemauricio.com>
2021-11-20 10:58:55 -0300

    React and Next installed.

---
+++ frontend_react/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+
+const App = () => {
+  return (<div>Hello changed more change</div>);
+};
+
+ReactDom.render(
+    <App />,
+    document.getElementById('root')
+);
