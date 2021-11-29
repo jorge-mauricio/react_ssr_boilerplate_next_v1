a1d956620412616fed99df70d0196f90fe2024fc
jorge-mauricio <web@jorgemauricio.com>
2021-11-29 09:31:22 -0300

    Adjustments.

--- next.config.js
+++ next.config.js
@@ -1,23 +1,30 @@
 'use strict';
+// import gSystemConfig from './frontend_react_next/config-application.js';
+// const { gSystemConfig } = require('./frontend_react_next/config-application.js');
+// const withTM = require('next-transpile-modules')(['config-application.js']);
 // import { gSystemConfig } from './frontend_react_next/config-application.js';
 // const { gSystemConfig } = require('./frontend_react_next/config-application.js');
 
 module.exports = {
+  // generateEtags: false,
+  // distDir: 'build',
+
+  // Alias
+  async rewrites() {
+    return [
+      {
+        // source: '/' + gSystemConfig.configDirectoryFilesLayoutSD + '/:files',
+        source: '/files-layout/:files',
+        destination: '/_next/static/media/:files',
+      }
+    ];
+  },
+
+  // Environment variables.
   env: {
     // System configuration.
     NEXT_APP_CONFIG_PORT: '3002',
     CONFIG_SYSTEM_URL: 'http://localhost:3002',
     CONFIG_SYSTEM_URL_SSL: 'http://localhost:3002',
   },
-  // Alias
-  /*
-  async rewrites() {
-    return [
-      {
-        source: '/' + gSystemConfig.configDirectoryFilesLayoutSD + '/:files',
-        destination: '/_next/static/media/:files',
-      }
-    ];
-  },
-  */
 };
