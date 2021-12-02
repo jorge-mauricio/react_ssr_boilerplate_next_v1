31d2f07ed8cb639370c652e11f9e3cb94dc0244e
jorge-mauricio <web@jorgemauricio.com>
2021-12-02 17:47:35 -0300

    TypeScript example.

---
+++ frontend_react_next/pages/example-typescript.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+const ExampleTypeScript: NextPage = () => {
+  return (
+    <div>
+      Hello TypeScript
+    </div>
+  );
+};
+
+export default ExampleTypeScript;
